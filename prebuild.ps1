01a0f773422358e277dc7ad9581ae6d1b878e877
Robert Holt <rjmholt_msft@outlook.com>
2018-10-31 14:14:53 -0700

    Make git installation work off Windows

--- prebuild.ps1
+++ prebuild.ps1
@@ -13,7 +13,21 @@ function Install-InvokeBuild
 
 function Install-Git
 {
-    $gitInstallerName = if ($IsWindows) { 'install-git.exe' } else { 'install-git' }
+    if ($IsLinux)
+    {
+        apt install -y git
+        return
+    }
+
+    if ($IsMacOS)
+    {
+        if (-not (which git))
+        {
+            throw "git installation required for installation to proceed..."
+        }
+    }
+
+    $gitInstallerName = 'install-git.exe'
     $gitInstallerPath = Join-Path $script:tmpdir $gitInstallerName
     Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/Git-2.19.1-32-bit.exe' -OutFile $gitInstallerPath
     Start-Process -Wait $gitInstallerPath -ArgumentList '/silent'
