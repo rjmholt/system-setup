4b377a9c1daa556cb153ffc6e1282f31c2e40d9a
Robert Holt <rjmholt_msft@outlook.com>
2018-11-01 21:14:59 -0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -4,6 +4,9 @@ $installScript = [System.IO.Path]::Combine($tmpdir, 'install-powershell.ps1')
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.ps1' -OutFile $installScript
 & $installScript
 
+# Update the path to include pwsh
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 $preBuildPath = [System.IO.Path]::Combine($tmpdir, 'prebuild.ps1')
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rjmholt/system-setup/master/prebuild.ps1' -OutFile $preBuildPath
 
