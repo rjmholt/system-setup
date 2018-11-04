3c62cf5f1e4f086e1eb69ce74f5f538b3956a759
Robert Holt <rjmholt_msft@outlook.com>
2018-11-03 23:13:01 -0700

    Fix profile path additions

--- profile.ps1
+++ profile.ps1
@@ -6,10 +6,10 @@ if ($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6)
     Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
     # Get ildasm on the path
-    $env:PATH += ';Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools'
+    $env:PATH += ';C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.1 Tools'
 
     # Put vim on the path
-    $env:PATH += ';Program Files (x86)\Vim\vim81'
+    $env:PATH += ';C:\Program Files (x86)\Vim\vim81'
 }
 
 Import-Module posh-git
